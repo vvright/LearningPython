cc06fd3cef41aed0249eeb148876ded98176c4e7
Gavriche WANG <vvright@163.com>
2018-08-31 17:17:04 +0800

    课堂范例

--- xinhuanet/flask/default.py
+++ xinhuanet/flask/default.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask
-
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
@@ -11,7 +10,7 @@ def default():
     <!DOCTYPE html>
     <html>
     <head>
-        <title>课程主页</title>
+        <title>博客主页</title>
         <style>
         .center {
             margin: auto;
@@ -43,43 +42,61 @@ def default():
         </div>
         <div class="center">
               <p><b>注意: </b>使用 margin:auto 无法兼容 IE8, 除非 !DOCTYPE 已经声明。</p>
-              <form action="">
+              <form action="/login" method="post">
               <table border="1">
                 <tr>
                     <th>标题</th>
-                    <th>Header 2</th>
+                    <th>内容</th>
                 </tr>
                 <tr>
-                    <td>课程：</td>
-                    <td><input name=""></td>
+                    <td>用户：</td>
+                    <td><input type="text" name="username"></td>
                 </tr>
                 <tr>
-                    <td>row 2, cell 1</td>
-                    <td><input name=""></td>
+                    <td>密码：</td>
+                    <td><input type="password" name="password"></td>
                 </tr>
                  <tr>
-                    <td><input type="submit" name="submit"></td>
-                    <td><input type="button" name="cancel" value="取消"></td>
+                    <td>
+                    <input type="submit" name="submit">
+                    <input type="button" name="cancel" value="取消" onclick="#">
+                    </td>
                 </tr>
             </table>
             </form>
+        </div>
+        <div class="center">
+            <a href="/articles">查看所有文章</a>
         </div>
     </body>
     </html>
     '''
 
 
-#    '''
-#    \<html\>
-#        \<head\>default\<\/head\>
-#    \<body\>
-#        \<div\>
-#            \<center\>北京林业大学信息学院\<\/center\>
-#        \<\/div\>
-#    \<\/body\>
-#    \<\/html\>
-#    '''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == u'北京林业大学':
+            if request.form['password'] == u'123456':
+                return u"欢饮您" + request.form['username']
+            else:
+                return request.form['username'] + u'密码不正确'
+        else:
+            return u'该用户不存在：'+ request.form['username']
+    else:
+        return u'HTTP方法不正确'
 
+
+@app.route('/articles')
+def show_articles():
+    return render_template('articles.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+#    '''
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port='80')
